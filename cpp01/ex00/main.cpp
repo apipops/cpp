c598cbcca1d0a9362529c557aee4fe5a6ad3e173
apipops <apollinevast@gmail.com>
2023-05-26 15:14:15 +0200

    cpp01/ex00

---
+++ cpp01/ex00/main.cpp
@@ -0,0 +1,18 @@
+#include "Zombie.hpp"
+
+int	main(void)
+{
+	std::cout << "Calling function 'newZombie'..." << std::endl;
+	// Allocation du zombie Rose dans la heap
+	Zombie *rose = newZombie("Rose");
+
+	// Allocation du zombie Jack dans la stack
+	// dans le scope de la fonction
+	std::cout << "Calling function 'randomChump'..." << std::endl;
+	randomChump("Jack");
+
+	rose->announce(); // Rose existe en dehors de sa fonction de création
+	delete rose; // Destructeur appelé seulement à la libération de la mémoire
+
+	return (0);
+}
